1fa448085a0fc83956afa9b8eaaa8c536bcb1af0
Max han <mwellh84@gmail.com>
2017-11-14 04:02:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file
+// function findMatching(array, name ){
+//   let matches = []
+//   for(const driver of array){
+//     if (driver.toLowerCase() === name.toLowerCase()){
+//       matches.push(driver)
+//     }
+//   }
+//   return matches
+// }
+
+//using the actual filter function below
+function findMatching(drivers, name){
+  let matchingDrivers = drivers.filter(function(driver){
+    if(name.toLowerCase() === driver.toLowerCase()){
+      return true
+    }else{
+      return false
+    }
+  })
+  return matchingDrivers
+}
+
+function fuzzyMatch(array, criteria ){
+  let matches = []
+  for(const driver of array){
+    if (driver.toLowerCase().indexOf(criteria.toLowerCase()) === 0) {
+      matches.push(driver)
+    }
+  }
+  return matches
+}
+
+
+// filter on an object
+function matchName (list, name) {
+  return list.filter(function (driver) {
+    return driver.name.toLowerCase() === name.toLowerCase();
+  });
+}
