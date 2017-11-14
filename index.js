9a1ad3668d56b0abd9b53d7e08e8085d996d45f0
Max han <mwellh84@gmail.com>
2017-11-14 03:52:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file
+function findMatching(array, name ){
+  let matches = []
+  for(const driver of array){
+    if (driver.toLowerCase() === name.toLowerCase()){
+      matches.push(driver)
+    }
+  }
+  return matches
+}
+
+function fuzzyMatch(array, criteria ){
+  let matches = []
+  for(const driver of array){
+    if (driver.toLowerCase().indexOf(criteria.toLowerCase()) === 0) {
+      matches.push(driver)
+    }
+  }
+  return matches
+}
